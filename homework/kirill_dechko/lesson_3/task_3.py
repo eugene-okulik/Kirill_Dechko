2dc1c6989ee8cffb944f6b18b566b7d9d22fedcf
Kirill Dechko <kirilldechko522@gmail.com>
2024-02-18 12:54:14 +0300

    Решение домашки lesson_3

--- homework/kirill_dechko/lesson_3/task_3.py
+++ homework/kirill_dechko/lesson_3/task_3.py
@@ -2,5 +2,5 @@ num_one = 4
 num_two = 13
 avg_nums = (num_one + num_two) / 2
 print(avg_nums)
-avg_geom_nums = (num_one * num_two) ** (1 / 2)  # Произведение всех значений в степени 1/(деленное на) количество значений
+avg_geom_nums = (num_one * num_two) ** (1 / 2)  # Произвед. всех значений в степени 1/(деленное на) количество значений
 print(avg_geom_nums)
