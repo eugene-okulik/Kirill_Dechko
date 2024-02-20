a4ee093ca469c99685cfa04d2b9d0c4da9b16d80
kirilldechko <100310727+kirilldechko@users.noreply.github.com>
2024-02-20 11:49:36 +0300

    lesson_4 practice with data types

--- homework/kirill_dechko/lesson_4/task_1.py
+++ homework/kirill_dechko/lesson_4/task_1.py
@@ -4,14 +4,15 @@ my_dict = {
     'dict': {'key_one': 'key', 'key_three': 3, 'key_four': 4, 'key_five': 5, 'key_six': 6},
     'set': {'name', 'surname', 'your_name', 'your_surname', 'any_value'}
 }
-#  print(my_dict['tuple'][-1])
+print(my_dict['tuple'][-1])
 my_dict['list'].append(7)
 my_dict['list'].pop(1)
 #  print(my_dict['list'])
-my_dict['dict']['i am a tuple'] = '2805'
+my_dict['dict']['i am a tuple'] = (2, 8, 0, 5)
 my_dict['dict'].pop('key_one')
 #  print(my_dict['dict'])
 my_dict['set'].add('phone')
 my_dict['set'].discard('your_name')
 #  print(my_dict['set'])
 print(my_dict)
+#  print(type(my_dict['dict']['i am a tuple']))
